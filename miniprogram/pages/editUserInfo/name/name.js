5a3c1d1a767fa9ccf099a0a449a72bb9639e95ba
laoxia123 <1452611972@qq.com>
2020-09-01 10:17:48 +0800

    third commit

--- miniprogram/pages/editUserInfo/name/name.js
+++ miniprogram/pages/editUserInfo/name/name.js
@@ -1,11 +1,14 @@
 // miniprogram/pages/editUserInfo/name/name.js
+const app = getApp()
+const db = wx.cloud.database()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+      nickName : ''
   },
 
   /**
@@ -19,7 +22,9 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+      this.setData({
+          nickName : app.userInfo.nickName
+      });
   },
 
   /**
@@ -62,5 +67,39 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  handleText(ev){
+    let value = ev.detail.value
+    this.setData({
+        nickName : value
+    });
+  },
+  handleBtn(ev){
+      this.updateNickName();
+  },
+  updateNickName(){
+    wx.showLoading({
+      title: '更新中',
+    })
+    db.collection("users").doc(app.userInfo._id).update({
+        data : {
+            nickName : this.data.nickName
+        }
+    }).then((res)=>{
+        wx.hideLoading();
+        wx.showToast({
+          title: '更新完成',
+        })
+    });
+  },
+  bindGetUserInfo(ev){
+      let userInfo = ev.detail.userInfo;
+      if(userInfo){
+        this.setData({
+            nickName : userInfo.nickName
+        },()=>{
+            this.updateNickName();
+        });
+      }
   }
 })
